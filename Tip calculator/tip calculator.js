edb0ceb888f97391cc4dbccf5d13114c7a337715
koolking1217 <ragav1282005@gmail.com>
2023-07-09 09:33:17 +0530

    tip calculator app

---
+++ Tip calculator/tip calculator.js
@@ -0,0 +1,16 @@
+let x = document.getElementById('input fot tip');
+
+let y= 0.25;
+
+let z=x*y;
+
+/*document.getElementById("final tip").textContent= z;
+
+document.getElementById("tip amount").textContent = "The total amount for tips is" , z;   */
+
+//document.getElementById("button").innerHTML=;
+function addparagraphText()
+{
+	document.getElementById("tip amount").innerHTML = "The total amount for tips is" ,z ;
+	
+}
